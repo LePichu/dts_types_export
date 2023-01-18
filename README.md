badcbbdaa2b70abf52d9e6de75e8776474e80cfd
lucsoft <mail@lucsoft.de>
2023-01-18 21:11:10 +0100

    Added prototype code of dts_typing_export

---
+++ README.md
@@ -0,0 +1,13 @@
+# dts_types_export
+
+A prototype for generating dts files from Deno.
+
+By current implmentation this will create a temp folder while using dnt.
+
+## Usage
+
+```ts
+import * as dts_typing_export from "https://deno.land/x/dts_typing_export/mod.ts";
+
+await dts_typing_export.generate();
+```
---
+++ mod.ts
@@ -0,0 +1,58 @@
+import { walk } from "https://deno.land/std@0.171.0/fs/walk.ts";
+import { build } from "https://deno.land/x/dnt@0.33.0/mod.ts";
+
+export type Options = {
+    /**
+     * Used by dnt. Gets removed when done.
+     */
+    tempFolder: string,
+    source: string | string[];
+    /**
+     * Path or url to import map. (Usefull when having bad imports. like css imports or other non valid web imports to point to an empty javascript file)
+     */
+    importMap?: string;
+};
+
+export type Path = string;
+export type Content = string;
+
+export async function generate({
+    tempFolder = "dts_typing_export/declaration",
+    source = "./mod.ts",
+    importMap
+}: Options) {
+    await build({
+        packageManager: "true",
+        test: false,
+        skipSourceOutput: true,
+        package: {
+            name: "dts_typing_export",
+            version: "1.0.0"
+        },
+        compilerOptions: {
+            lib: [ "dom", "esnext" ]
+        },
+        importMap: importMap,
+        shims: {},
+        outDir: tempFolder,
+        entryPoints: typeof source == "string" ? [ source ] : source
+    });
+    const declarations = new Map<Path, Content>();
+
+    for await (const iterator of walk(`./${tempFolder}/declaration/types`)) {
+        if (iterator.name.startsWith("_") || !iterator.name.endsWith(".d.ts")) continue;
+
+        const key = iterator.path.replace(`${tempFolder}/declaration/types/`, "");
+
+        // I don't know why dnt does this. Sure its a bug. Keep in mind when updating dnt.
+        const value = Deno.readTextFileSync(iterator.path)
+            .replaceAll(`.js';`, `.d.ts';`)
+            .replaceAll(`.js";`, `.d.ts";`);
+
+        declarations.set(key, value);
+    }
+
+    await Deno.remove(tempFolder, { recursive: true });
+
+    return declarations;
+}
